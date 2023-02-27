8eb2f312fef8f3523de1061984d9d05a83538e95
aquaye11 <albertjohnsonquaye@gmail.com>
2023-02-27 03:57:12 -0800

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
-include "main.h"
+#include "main.h"
 /**
 *reset_to_98 - reset argument to 98
 *@n: printed integer
 *Return: void
 */
-void reset_to_98(int *n) 
+ void reset_to_98(int *n) 
 { 
 
     *n = 98;
